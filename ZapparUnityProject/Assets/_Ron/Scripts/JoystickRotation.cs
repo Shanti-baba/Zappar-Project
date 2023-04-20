7ef14ee32c7d04f6fe2a7b5113fc25ff5431817b
RonS <RonS@oversight-ar.com>
2023-04-20 18:42:24 +0300

    Billboard Update

---
+++ ZapparUnityProject/Assets/_Ron/Scripts/JoystickRotation.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class JoystickRotation : MonoBehaviour
+{
+    public Joystick joystick;
+    public float rotateHorizontal;
+    public float rotationSpeed = 10f;
+
+    private void FixedUpdate()
+    {
+        rotateHorizontal = joystick.Horizontal * -1f;
+        Quaternion rotation = Quaternion.Euler(0, rotateHorizontal * rotationSpeed, 0);
+        transform.localRotation *= rotation;
+    }
+}
